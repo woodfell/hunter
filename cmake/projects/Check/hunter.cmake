9af5d6b46b16c6f76c738b638406f68f2e469e28
Matt Woodward <matthew.woodward@swift-nav.com>
2019-11-06 12:36:08 +1100

    Import libcheck

---
+++ cmake/projects/Check/hunter.cmake
@@ -0,0 +1,29 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+  PACKAGE_NAME
+  Check
+  VERSION
+  "0.13.0"
+  URL
+  "https://github.com/libcheck/check/releases/download/0.13.0/check-0.13.0.tar.gz"
+  SHA1
+  03d540c4c2a0e245f2eae5757711a4bd7409866b
+  )
+
+hunter_cmake_args(
+  Check
+  CMAKE_ARGS
+  BUILD_TESTING=OFF
+  CHECK_ENABLE_GCOV=OFF
+  ENABLE_MEMORY_LEAKING_TESTS=OFF
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+hunter_cacheable(Check)
+hunter_download(PACKAGE_NAME Check)
+
